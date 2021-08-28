1654f15ad13bebf16ccb25cba0aaab9d5de67f48
Manthan Vaghani <manthanvaghani101@gmail.com>
2021-08-28 11:58:10 +0530

    The method can take up to two numbers, separated by commas, and will return their sum, example “” or “1” or “1,2” as inputs.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,17 @@
+import com.google.common.base.Splitter;
+
+import java.util.List;
+
 public class StringCalculator {
+
+    public int add(String inputString) {
+        if("".equalsIgnoreCase(inputString)){
+            return 0;
+        }
+        List<String> StringNumbers = Splitter.onPattern(",").splitToList(inputString);
+        if(StringNumbers.size() == 1){
+            return Integer.valueOf(inputString);
+        }
+        return Integer.valueOf(StringNumbers.get(0)) + Integer.valueOf(StringNumbers.get(1));
+    }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @Before
+    public void setUp(){
+        stringCalculator = new StringCalculator();
+    }
+
+    @Test
+    public void ForEmptyString(){
+        int sum = stringCalculator.add("");
+        assertEquals(0, sum);
+    }
+
+    @Test
+    public void ForSingleLatter(){
+        int sum = stringCalculator.add("1");
+        assertEquals(1, sum);
+    }
+
+    @Test
+    public void ReturnSumOfTwoDigit(){
+        int sum = stringCalculator.add("5,10");
+        assertEquals(15, sum);
+    }
+
 }
